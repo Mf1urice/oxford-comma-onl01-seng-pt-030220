114934d38b21cebb12b6e98504727da00c2a4108
Maurice Falls <falls8019oct@gmail.com>
2020-03-15 23:36:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,8 +2,13 @@ def oxford_comma(array)
     if array.size == 1
       array[0]
     elsif array.size == 2 
+<<<<<<< HEAD
     array.join(" and ")
     else
+=======
+    array.join("," "and", ",")
+  else
+>>>>>>> c57d4f3b9067d5f6690ed23b1891fc36feae550d
     return array[0..-2].join(', ') + ", and " + array[-1]
 end
 end
